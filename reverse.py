45daeb5c5e0a071eaa3315bb0b471a2f31c5d70d
kmein <kieran.meinhardt@gmail.com>
2016-10-03 23:29:51 +0200

    Simplify reverse.py

--- reverse.py
+++ reverse.py
@@ -2,16 +2,13 @@
 
 import sys
 import telepot
-from telepot.delegate import per_chat_id, create_open, pave_event_space
-class Reverser(telepot.helper.ChatHandler):
-    def __init__(self, *args, **kwargs):
-        super(Reverser, self).__init__(*args, **kwargs)
 
-    def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        self.sender.sendMessage(msg["text"][::-1], reply_to_message_id=msg["message_id"])
+def handle(msg):
+    content_type, _, chat_id, _, message_id = telepot.glance(msg, long=True)
+    if content_type == "text":
+        bot.sendMessage(chat_id, msg["text"][::-1], reply_to_message_id=message_id)
 
 TOKEN = open("reverse.token").read().strip()
 
-bot = telepot.DelegatorBot(TOKEN, [pave_event_space()(per_chat_id(), create_open, Reverser, timeout=10)])
-bot.message_loop(run_forever='Listening ...')
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle, run_forever="Listening ...")
