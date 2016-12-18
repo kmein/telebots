61fdc764475333da440595539da9dbf9a4ec8262
kmein <kieran.meinhardt@gmail.com>
2016-12-18 21:33:49 +0100

    Make betacode react only to inline queries

--- betacode.py
+++ betacode.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 from cltk.corpus.greek.beta_to_unicode import Replacer
+from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent
 import bcolors
 import logging
 import telepot
 
 replacer = Replacer()
 
-def handle(msg):
-    content_type, _, chat_id = telepot.glance(msg)
-    logging.info("Received a {} in chat {}".format(content_type, chat_id))
-    if content_type == "text":
-        text = msg["text"]
-        reply = replacer.beta_code(text.upper())
-        logging.info("Message is a {}, namely \"{}\"".format(content_type, text))
-        logging.info("Sending response \"{}\" in chat {}".format(reply, chat_id))
-        bot.sendMessage(chat_id, reply)
-    logging.info("Done")
+def on_inline_query(msg):
+    try:
+        query_id, _, query_string = telepot.glance(msg, flavor="inline_query")
+        greek = replacer.beta_code(query_string.upper())
+
+        logging.info("Message is query #{}, namely \"{}\"".format(query_id, query_string))
+
+        articles = [InlineQueryResultArticle(
+            id="betacode",
+            title=greek,
+            input_message_content=InputTextMessageContent(message_text=greek)
+            )]
+
+        bot.answerInlineQuery(query_id, articles)
+        logging.info("Proposing {}".format(greek))
+    except telepot.exception.TelegramError:
+        pass
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -27,4 +35,5 @@ if __name__ == "__main__":
     TOKEN = open("betacode.token").read().strip()
 
     bot = telepot.Bot(TOKEN)
-    bot.message_loop(handle, run_forever=True)
+    bot.message_loop({"inline_query": on_inline_query}, run_forever=True)
+
