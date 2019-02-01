e83ec7d78f24214801d53cc3706918d282d9cadf
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-02-01 01:52:40 +0100

    telegram-reverse: improve naming

--- telegram-reverse/setup.py
+++ telegram-reverse/setup.py
@@ -1,2 +1,8 @@
 from setuptools import setup
-setup(scripts=["./telegram-reverse"])
+
+setup(
+    name="telegram-reverse",
+    version="0.2.0",
+    scripts=["./telegram-reverse"],
+    install_requires=["pillow", "pydub", "python-telegram-bot"],
+)
