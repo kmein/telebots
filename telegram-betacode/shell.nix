0e0da1ca789ce5d552e69182bcd981d8b74bf46a
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-02-06 08:39:40 +0100

    telegram-betacode: handle text messages

--- telegram-betacode/shell.nix
+++ telegram-betacode/shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
+with pkgs.python3Packages;
 let
   pygtrie =
-    with pkgs.python3Packages;
     buildPythonPackage rec {
       pname = "pygtrie";
       version = "2.3";
@@ -11,16 +11,17 @@ let
       };
       doCheck = false;
     };
-  betacode =
-    with pkgs.python3Packages;
-    buildPythonPackage rec {
+  betacode = buildPythonPackage rec {
       pname = "betacode";
       version = "0.2";
       src = fetchPypi {
         inherit pname version;
         sha256 = "08fnjzjvnm9m6p4ddyr8qgfb9bs2nipv4ls50784v0xazgxx7siv";
       };
+      preBuild = ''sed -i 's/[\d128-\d255]//g' ./README.rst'';
       propagatedBuildInputs = [ pygtrie ];
       doCheck = false;
     };
-in (pkgs.python3.withPackages (py: [py.python-telegram-bot betacode])).env
+in (pkgs.python3.withPackages (py: [py.python-telegram-bot betacode])).env // {
+  TELEGRAM_BETACODE_TOKEN = builtins.readFile ./Tokenfile;
+}
