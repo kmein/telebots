dfd934b13887cfae0df4dcd5e6c190fb3aaf6832
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-05-03 21:23:04 +0200

    telegram-odyssey: restructure

--- telegram-odyssey/setup.py
+++ telegram-odyssey/setup.py
@@ -4,7 +4,7 @@ import glob
 setup(
     name="telegram_odyssey",
     version="0.1.0",
-    scripts=["telegram-odyssey"],
+    scripts=["src/telegram-odyssey"],
     data_files=[("odysseia", glob.glob("odysseia/*.txt"))],
     install_requires=["python-telegram-bot"],
 )
